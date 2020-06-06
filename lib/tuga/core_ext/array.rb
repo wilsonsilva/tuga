6cda528568c7fecb2762803a3a1ac8d10969eb59
Wilson Silva <wilson.dsigns@gmail.com>
2020-06-06 18:03:11 +0100

    Document the array translations

--- lib/tuga/core_ext/array.rb
+++ lib/tuga/core_ext/array.rb
@@ -2,10 +2,21 @@
 
 # Portuguese translations
 class Array
+  # Reverte os elementos da lista
   alias inverter reverse
+
+  # Retorna o ultimo elemento da lista
   alias ultimo last
+
+  # Retorna o primeiro elemento da lista
   alias primeiro first
+
+  # Itera sobre cada elemento da lista
   alias cada each
+
+  # Itera sobre cada elemento da lista com um indice
   alias cada_com_indice each_with_index
+
+  # Retorna o numero de elementos da lista
   alias comprimento length
 end
